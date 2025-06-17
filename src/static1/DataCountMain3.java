eee4ff539676eafad622fb9737639e3bdffcb0f1
byeongjun98 <degea98@gmail.com>
2025-06-17 14:22:07 +0900

    [Feat] 55. static 변수3

--- src/static1/DataCountMain3.java
+++ src/static1/DataCountMain3.java
@@ -10,5 +10,14 @@ public class DataCountMain3 {
 
         Data3 data3 = new Data3("C");
         System.out.println("C count = " + Data3.count);
+
+        // 인스턴스를 통한 접근
+        // 권장하지 않는다. 인스턴스 변수인지 정적 변수 (static) 인지 구분하기에 용이함을 위해
+        Data3 data4 = new Data3("D");
+        System.out.println("D count = " + data4.count);
+
+        // 클래스를 통한 접근
+        Data3 data5 = new Data3("E");
+        System.out.println("E count = " + Data3.count);
     }
 }
