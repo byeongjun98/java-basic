139259bef68573ffcda26bf98de071d53a27a675
byeongjun98 <degea98@gmail.com>
2025-07-14 16:13:56 +0900

    [Feat] 93. 다형성 - 역할과 구현 예제2

--- src/poly/car0/CarMain0.java
+++ src/poly/car0/CarMain0.java
@@ -7,5 +7,11 @@ public class CarMain0 {
         System.out.println("K3Car 객체 생성" );
         driver.setK3Car(k3Car);
         driver.drive();
+
+        Model3Car model3Car = new Model3Car();
+        System.out.println("Model3Car 객체 생성" );
+        driver.setK3Car(null);
+        driver.setMdel3Car(model3Car);
+        driver.drive();
     }
 }
---
+++ src/poly/car0/Model3Car.java
@@ -0,0 +1,15 @@
+package poly.car0;
+
+public class Model3Car {
+    public void startEngine() {
+        System.out.println("Mdel3Car 시동을 걸었습니다.");
+    }
+
+    public void offEngine() {
+        System.out.println("Mdel3Car 시동을 끄었습니다.");
+    }
+    public void pressAccelerator() {
+        System.out.println("Mdel3Car 가속기를 밟았습니다.");
+    }
+
+}
